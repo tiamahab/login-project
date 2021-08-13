94e5dd5300490a2f74df29a491078ebf83f53ee6
tiamahab <87531455+tiamahab@users.noreply.github.com>
2021-08-13 19:49:16 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project</title>
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="all.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+    <body>
+        <div class="wrapper">
+            <div class="icon"><i class="fas fa-user"></i></div>
+            <div class="content">
+                <h4>Welcome</h4>
+                <p>to my simple project</p>
+            </div>
+            <div class="buttons">
+                <button><a href="login.php">Login</a></button>
+                <button><a href="signup.php">Signup</a></button>
+            </div>
+        </div>
+
+       <script src="jquery.1.min.js"></script>
+       <script src="bootstrap.min.js"></script>
+       <script src="all.js"></script>
+</body>
+</html>
