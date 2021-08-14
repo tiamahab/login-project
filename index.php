d6009418f8767acb5f6b8b3fafb174e41ac81491
tiamahab <87531455+tiamahab@users.noreply.github.com>
2021-08-14 15:29:07 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,44 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project</title>
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="all.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+    <body>
+        <div class="wrapper">
+            <div class="icon"><i class="fas fa-user"></i></div>
+            <div class="content">
+                <br>
+                <br>
+                <br>
+                <h4>Bet against each other <br>
+                    rather than robbing bet <br>
+                    companies
+                </h4>
+                <br>
+                <br>
+                <br>
+                <p>When betting with a betting company you <br>
+                    can only bet on the odds they offer, which <br>
+                    includes their margin - sometimes as high <br>
+                    as 20% - meaning they price markets in <br>
+                    their favour with unfair odds
+                </p>
+            </div>
+            <div class="buttons">
+                <button><a href="login.php">Login</a></button>
+                <button><a href="signup.php">Signup</a></button>
+            </div>
+        </div>
+
+       <script src="jquery.1.min.js"></script>
+       <script src="bootstrap.min.js"></script>
+       <script src="all.js"></script>
+</body>
+</html>
