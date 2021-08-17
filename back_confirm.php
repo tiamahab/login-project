77527bad4c36e301a5a7e638d45d38f814f77333
tiamahab <87531455+tiamahab@users.noreply.github.com>
2021-08-17 17:28:23 +0300

    Update back_confirm.php

--- back_confirm.php
+++ back_confirm.php
@@ -5,11 +5,11 @@
     }
 
     //connect to the database
-    $conn = mysqli_connect("localhost","root", "","brian");
-    //checking connection
-    if (!$conn) {
-          die("connection failed: " . mysqli_connect_error());
-        }
+$conn=mysqli_connect("sql5.freesqldatabase.com","sql5430872","QRNB4IUK2P","sql5430872");
+if(!$conn){
+die("connection failed:" . mysqli_connect_error());
+}
+
     $kind=$_SESSION['kind'];
     $id=$_SESSION['matchid'];
 
