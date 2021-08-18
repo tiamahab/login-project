49f5675aaa70b7c7fef9c7995b81ec000b43c3b9
tiamahab <87531455+tiamahab@users.noreply.github.com>
2021-08-18 16:52:00 +0300

    Update admin.login.php

--- admin.login.php
+++ admin.login.php
@@ -1,11 +1,7 @@
 <?php 
     session_start();
     //connect to the database
-    $conn = mysqli_connect("localhost","root", "","brian");
-    //checking connection
-    if (!$conn) {
-      die("connection failed: " . mysqli_connect_error());
-    } 
+    $conn=mysqli_connect("sql5.freesqldatabase.com","sql5430872","QRNB4IUK2P","sql5430872");if(!$conn){die("connection failed:" . mysqli_connect_error());}
 
     //login
     if(isset($_POST['login'])){
