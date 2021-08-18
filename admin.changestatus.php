0cad4a8b8ed7ea0b1127c36a4a6ecf2edd7351f7
tiamahab <87531455+tiamahab@users.noreply.github.com>
2021-08-18 16:49:45 +0300

    Update admin.changestatus.php

--- admin.changestatus.php
+++ admin.changestatus.php
@@ -1,10 +1,6 @@
 <?php
         //connect to the database
-        $conn = mysqli_connect("localhost","root", "","brian");
-        //checking connection
-        if (!$conn) {
-              die("connection failed: " . mysqli_connect_error());
-        }
+        $conn=mysqli_connect("sql5.freesqldatabase.com","sql5430872","QRNB4IUK2P","sql5430872");if(!$conn){die("connection failed:" . mysqli_connect_error());}
     $status=$_GET['status'];
     $id=$_GET['id'];
     if($status=='deactivate'){
