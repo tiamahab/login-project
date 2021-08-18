244de45b41670d3269da8e3d00f90c26adb0df17
tiamahab <87531455+tiamahab@users.noreply.github.com>
2021-08-18 16:51:08 +0300

    Update admin.editmatch.php

--- admin.editmatch.php
+++ admin.editmatch.php
@@ -4,11 +4,7 @@
         header('location: logout.php');
     }
     //connect to the database
-    $conn = mysqli_connect("localhost","root", "","brian");
-    //checking connection
-    if (!$conn) {
-        die("connection failed: " . mysqli_connect_error());
-    }
+    $conn=mysqli_connect("sql5.freesqldatabase.com","sql5430872","QRNB4IUK2P","sql5430872");if(!$conn){die("connection failed:" . mysqli_connect_error());}
     $id=$_GET['id'];
     $sql="SELECT * FROM matches WHERE id='".$id."'";
     $result= mysqli_query($conn, $sql);
