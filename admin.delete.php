078813c6f349f51a4ea1b9c3d191dc582585ea4a
tiamahab <87531455+tiamahab@users.noreply.github.com>
2021-08-21 13:39:45 +0300

    Update admin.delete.php

--- admin.delete.php
+++ admin.delete.php
@@ -1,6 +1,9 @@
 <?php
     //connect to the database
-    $conn=mysqli_connect("sql5.freesqldatabase.com","sql5430872","QRNB4IUK2P","sql5430872");if(!$conn){die("connection failed:" . mysqli_connect_error());}
+    $conn=mysqli_connect("sql4.freesqldatabase.com","sql4432075","Sr37jBwi8s","sql4432075");
+	if(!$conn){
+		die("connection failed:" . mysqli_connect_error());
+	}
     $id=$_GET['id'];
 
     $sql="DELETE FROM users WHERE id='".$id."'";
