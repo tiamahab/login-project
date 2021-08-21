55f598d7ce639a5311e6b0d18824f91dba11bdc5
tiamahab <87531455+tiamahab@users.noreply.github.com>
2021-08-21 14:09:11 +0300

    Update over_under_back.php

--- over_under_back.php
+++ over_under_back.php
@@ -5,7 +5,10 @@
     }
 
     //connect to the database
-    $conn=mysqli_connect("sql5.freesqldatabase.com","sql5430872","QRNB4IUK2P","sql5430872");if(!$conn){die("connection failed:" . mysqli_connect_error());}
+    $conn=mysqli_connect("sql4.freesqldatabase.com","sql4432075","Sr37jBwi8s","sql4432075");
+	if(!$conn){
+		die("connection failed:" . mysqli_connect_error());
+	}
 
     $kind=$_GET['kind'];
     $id=$_GET['matchid'];
