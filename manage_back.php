c377471dcdd436040c4881fc630a9a0499022092
tiamahab <87531455+tiamahab@users.noreply.github.com>
2021-08-21 13:59:24 +0300

    Update manage_back.php

--- manage_back.php
+++ manage_back.php
@@ -1,10 +1,10 @@
 <?php
     session_start();
     //connect to the database
-    $conn=mysqli_connect("sql5.freesqldatabase.com","sql5430872","QRNB4IUK2P","sql5430872");
-if(!$conn){
-die("connection failed:" . mysqli_connect_error());
-}
+    $conn=mysqli_connect("sql4.freesqldatabase.com","sql4432075","Sr37jBwi8s","sql4432075");
+	if(!$conn){
+		die("connection failed:" . mysqli_connect_error());
+	}
     $sqluser="SELECT * FROM users WHERE contact ='".$_SESSION['phone']."'";
     $res=mysqli_query($conn, $sqluser);
     $rowuser=mysqli_fetch_assoc($res);
